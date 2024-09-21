92fa3ca4689197158647d275f08e1463f892296b
bluw6525 <suryaok1982@gmail.com>
2024-09-21 19:32:48 +0700

    adding categories header, deleting @media 630px and changing categories to explore in main header

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+window.addEventListener('scroll', () => {
+    const limiter = document.querySelector('.categories');  
+    const hidden = document.querySelector('.categories-header'); 
+    
+    const limiterPosition = limiter.getBoundingClientRect().bottom; 
+
+    if (limiterPosition <= 80) {
+        hidden.classList.add('categories-header-show'); 
+    } else {
+        hidden.classList.remove('categories-header-show');  
+    }
+});
