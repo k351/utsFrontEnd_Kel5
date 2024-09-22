84c213b2908679fe07dd957b39a3f3f6d800cec5
bluw6525 <suryaok1982@gmail.com>
2024-09-22 13:32:27 +0700

    adding explore header

--- script.js
+++ script.js
@@ -84,6 +84,34 @@ $(document).ready(function() {
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 var darkTheme = document.getElementById("dark-theme");
+const exploreMenu = document.getElementById("Explore");
+const exploreLinks = document.querySelector(".explore-links");
+const priaMenu = document.querySelector(".pria");
+const wanitaMenu = document.querySelector(".wanita");
+const priaExplore = document.getElementById("pria-contents");
+const wanitaExplore = document.getElementById("wanita-contents");
+
+priaMenu.addEventListener("click", () => {
+    priaExplore.classList.toggle("pria-contents-show")
+    if (wanitaExplore.classList.contains("wanita-contents-show")) {
+        wanitaExplore.classList.remove("wanita-contents-show");
+    }
+})
+
+wanitaMenu.addEventListener("click", () => {
+    wanitaExplore.classList.toggle("wanita-contents-show")
+    if (priaExplore.classList.contains("pria-contents-show")) {
+        priaExplore.classList.remove("pria-contents-show");
+    }
+})
+
+exploreMenu.addEventListener("click", () => {
+    exploreLinks.classList.toggle("explore-links-show");
+    if (wanitaExplore.classList.contains("wanita-contents-show") || priaExplore.classList.contains("pria-contents-show")) {
+        wanitaExplore.classList.remove("wanita-contents-show");
+        priaExplore.classList.remove("pria-contents-show");
+    }
+});
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -96,15 +124,23 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 }))
 
 window.addEventListener('scroll', () => {
-    const limiter = document.querySelector('.categories');  
-    const hidden = document.querySelector('.categories-header'); 
-    
-    const limiterPosition = limiter.getBoundingClientRect().bottom; 
+    const searchLimiter = document.querySelector('.home');
+    const searchHidden = document.querySelector('.search-header');
+    const categoriesLimiter = document.querySelector('.categories');  
+    const categoriesHidden = document.querySelector('.categories-header'); 
+    const searchLimiterPos = searchLimiter.getBoundingClientRect().bottom
+    const categoriesLimiterPos = categoriesLimiter.getBoundingClientRect().bottom; 
 
-    if (limiterPosition <= 80) {
-        hidden.classList.add('categories-header-show'); 
+    if (searchLimiterPos <= 80) {
+        searchHidden.classList.add('search-header-show'); 
     } else {
-        hidden.classList.remove('categories-header-show');  
+        searchHidden.classList.remove('search-header-show');  
+    }
+    
+    if (categoriesLimiterPos <= 80) {
+        categoriesHidden.classList.add('categories-header-show'); 
+    } else {
+        categoriesHidden.classList.remove('categories-header-show');  
     }
 });
 
