bab0993a5d11e0a40a35b7edce62e5dc99759263
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2024-09-24 22:50:35 +0700

    Save mode when page refreshed

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,10 +1,20 @@
 var darkTheme = document.getElementById("dark-theme");
 
+if(localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("darkmode");
+    darkTheme.src = "images/icon/sun.png";
+} else {
+    darkTheme.src = "images/icon/moon.png";
+}
+
 darkTheme.onclick = function() {
     document.body.classList.toggle("darkmode");
-    if(document.body.classList.contains("darkmode")){
+
+    if (document.body.classList.contains("darkmode")) {
         darkTheme.src = "images/icon/sun.png";
+        localStorage.setItem("theme", "dark"); 
     } else {
         darkTheme.src = "images/icon/moon.png";
+        localStorage.setItem("theme", "light"); 
     }
 }
